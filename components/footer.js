81b5d6d604610c204f30d0b7136446db4417be69
Iain Collins <me@iaincollins.com>
2020-06-26 09:38:12 +0100

    Update NPM link; reduce CSS

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default () => (
     <hr/>
     <ul className={styles.navItems}>
       <li className={styles.navItem}><a href="https://next-auth.js.org">Documentation</a></li>
-      <li className={styles.navItem}><a href="https://www.npmjs.com/package/next-auth/v/beta">NPM</a></li>
+      <li className={styles.navItem}><a href="https://www.npmjs.com/package/next-auth">NPM</a></li>
       <li className={styles.navItem}><a href="https://github.com/iaincollins/next-auth-example">GitHub</a></li>
     </ul>
   </div>

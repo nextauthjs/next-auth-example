f750ea5abaeeb5ff0526ea8c1ecb35861ff11be7
Iain Collins <me@iaincollins.com>
2020-07-08 10:39:37 +0100

    Update to 3.0.0-beta.15

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ export default () => (
       <li className={styles.navItem}><a href="https://next-auth.js.org">Documentation</a></li>
       <li className={styles.navItem}><a href="https://www.npmjs.com/package/next-auth">NPM</a></li>
       <li className={styles.navItem}><a href="https://github.com/iaincollins/next-auth-example">GitHub</a></li>
-      <li className={styles.navItem}><em>next-auth@3.0.0-beta.13</em></li>
+      <li className={styles.navItem}><em>next-auth@3.0.0-beta.15</em></li>
     </ul>
   </footer>
 )

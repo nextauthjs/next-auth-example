36244562c4826519228ea863b2355b3f924d8a2f
Iain Collins <me@iaincollins.com>
2020-07-15 00:42:28 +0100

    Combine Privacy Policy and Terms of Service
    Both are required by some providers.

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ export default () => (
       <li className={styles.navItem}><a href="https://next-auth.js.org">Documentation</a></li>
       <li className={styles.navItem}><a href="https://www.npmjs.com/package/next-auth">NPM</a></li>
       <li className={styles.navItem}><a href="https://github.com/iaincollins/next-auth-example">GitHub</a></li>
-      <li className={styles.navItem}><Link href="/privacy"><a>Privacy Policy</a></Link></li>
+      <li className={styles.navItem}><Link href="/policy"><a>Policy</a></Link></li>
       <li className={styles.navItem}><em>next-auth@3.0.0-beta.19</em></li>
     </ul>
   </footer>
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default () => (
   <Layout>
     <h1>NextAuth.js Example</h1>
     <p>
-      This is an example to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
+      This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
     </p>
   </Layout>
 )

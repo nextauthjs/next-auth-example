d0423552c602764939b52c63cadc456ceaba455c
Iain Collins <me@iaincollins.com>
2020-07-27 05:49:30 +0100

    Update repository link in footer

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,7 @@ export default function Footer () {
       <ul className={styles.navItems}>
         <li className={styles.navItem}><a href="https://next-auth.js.org">Documentation</a></li>
         <li className={styles.navItem}><a href="https://www.npmjs.com/package/next-auth">NPM</a></li>
-        <li className={styles.navItem}><a href="https://github.com/iaincollins/next-auth-example">GitHub</a></li>
+        <li className={styles.navItem}><a href="https://github.com/nextauthjs/next-auth-example">GitHub</a></li>
         <li className={styles.navItem}><Link href="/policy"><a>Policy</a></Link></li>
         <li className={styles.navItem}><em>{version}</em></li>
       </ul>

0be7b61844dd34f3b6a293edf2808f02328a97f9
Balázs Orbán <info@balazsorban.com>
2021-01-30 21:57:14 +0100

    use same version number as next-auth in package.json

--- components/footer.js
+++ components/footer.js
@@ -1,17 +1,29 @@
-import Link from 'next/link'
-import styles from './footer.module.css'
-import { version } from '../package.json'
+import Link from "next/link"
+import styles from "./footer.module.css"
+import { dependencies } from "../package.json"
 
-export default function Footer () {
+export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <hr/>
+      <hr />
       <ul className={styles.navItems}>
-        <li className={styles.navItem}><a href="https://next-auth.js.org">Documentation</a></li>
-        <li className={styles.navItem}><a href="https://www.npmjs.com/package/next-auth">NPM</a></li>
-        <li className={styles.navItem}><a href="https://github.com/nextauthjs/next-auth-example">GitHub</a></li>
-        <li className={styles.navItem}><Link href="/policy"><a>Policy</a></Link></li>
-        <li className={styles.navItem}><em>{version}</em></li>
+        <li className={styles.navItem}>
+          <a href="https://next-auth.js.org">Documentation</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="https://www.npmjs.com/package/next-auth">NPM</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="https://github.com/nextauthjs/next-auth-example">GitHub</a>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/policy">
+            <a>Policy</a>
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <em>next-auth@{dependencies["next-auth"]}</em>
+        </li>
       </ul>
     </footer>
   )

f80a7d3ce4aad7eacb692be1e7a08e5322047185
ndom91 <yo@ndo.dev>
2021-08-11 22:29:09 +0200

    fix: config comments

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -106,10 +106,10 @@ export default NextAuth({
   // when an action is performed.
   // https://next-auth.js.org/configuration/callbacks
   callbacks: {
-    // async signIn(user, account, profile) { return true },
-    // async redirect(url, baseUrl) { return baseUrl },
-    // async session(session, user) { return session },
-    // async jwt(token, user, account, profile, isNewUser) { return token }
+    // async signIn({ user, account, profile, email, credentials }) { return true },
+    // async redirect({ url, baseUrl }) { return baseUrl },
+    // async session({ session, token, user }) { return session },
+    // async jwt({ token, user, account, profile, isNewUser }) { return token }
   },
 
   // Events are useful for logging

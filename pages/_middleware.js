db89e5151bf49ab83bae1741176ddfeef74a8c93
Balázs Orbán <info@balazsorban.com>
2021-10-28 00:31:57 +0200

    feat: add Next.js 12 Middleware example page

---
+++ pages/_middleware.js
@@ -0,0 +1,17 @@
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+
+export async function middleware(req) {
+  // return early if url isn't supposed to be protected
+  if (req.url !== "/middleware-protected") {
+    return NextResponse.next()
+  }
+
+  const session = await getToken({ req, secret: process.env.SECRET })
+  // You could also check for any property on the session object,
+  // like rolec === "admin" or name === "John Doe", etc.
+  if (!session) return NextResponse.redirect("/api/auth/signin")
+
+  // If user is authenticated, continue.
+  return NextResponse.next()
+}

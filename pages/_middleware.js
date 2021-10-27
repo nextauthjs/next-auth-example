55d872a182fd6a5cb79e66a81f933e6438272567
Balázs Orbán <info@balazsorban.com>
2021-10-28 00:47:09 +0200

    chore: revert

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,18 +1,17 @@
 import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
 
 export async function middleware(req) {
   // return early if url isn't supposed to be protected
   if (req.url !== "/middleware-protected") {
-    return
+    return NextResponse.next()
   }
 
   const session = await getToken({ req, secret: process.env.SECRET })
   // You could also check for any property on the session object,
   // like rolec === "admin" or name === "John Doe", etc.
-  if (!session)
-    return new Response(undefined, {
-      headers: { Location: "/api/auth/signin" },
-    })
+  if (!session) return NextResponse.redirect("/api/auth/signin")
 
   // If user is authenticated, continue.
+  return NextResponse.next()
 }

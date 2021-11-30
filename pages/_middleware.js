86373fa0c27ad31f793aedc72daecc287f9a723e
Balázs Orbán <info@balazsorban.com>
2021-12-01 00:03:29 +0100

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,9 +4,7 @@ import { NextResponse } from "next/server"
 /** @param {import("next/server").NextRequest} req */
 export async function middleware(req) {
   if (req.nextUrl.pathname === "/middleware-protected") {
-    console.log("Checking session.")
     const session = await getToken({ req, secret: process.env.SECRET })
-    console.log(session)
     // You could also check for any property on the session object,
     // like role === "admin" or name === "John Doe", etc.
     if (!session) return NextResponse.redirect("/api/auth/signin")

2e3994c07ec88a2151f6d15f2874258da2524ab8
Nico Domino <yo@ndo.dev>
2021-12-01 13:41:08 +0100

    fix: comment out email adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-import EmailProvider from "next-auth/providers/email"
+// import EmailProvider from "next-auth/providers/email"
 import FacebookProvider from "next-auth/providers/facebook"
 import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
@@ -12,10 +12,10 @@ import Auth0Provider from "next-auth/providers/auth0"
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers
   providers: [
-    EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
+    // EmailProvider({
+    //   server: process.env.EMAIL_SERVER,
+    //   from: process.env.EMAIL_FROM,
+    // }),
     // Temporarily removing the Apple provider from the demo site as the
     // callback URL for it needs updating due to Vercel changing domains
     /*

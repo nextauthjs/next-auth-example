b42c128456e35db2adf72152f79523cbd21af217
Nico Domino <yo@ndo.dev>
2021-12-01 16:57:00 +0100

    feat: migrate to v4 (#40)
    * chore: bump nextjs to 11
    
    * feat: migrate to v4.0.0-next.20
    
    * fix: config comments
    
    * fix: add nodemailer as manual dependency
    
    * feat: add ndom91 as contributor :grin:
    
    * fix: provider import
    
    * feat: update next-auth
    
    * fix: add package-lock
    
    * feat: update next-auth to v4-beta.1
    
    * fix: remove carrot
    
    * feat: upgrade to beta.2
    
    * fix: filename typo
    
    * feat: add Next.js 12 Middleware example page
    
    * chore: simplify Middleware
    
    * chore: revert
    
    * chore(deps): ugprade `next-auth`
    
    * chore: change url check
    
    * chore: add logs, refactor middleware
    
    * fix: use new theme option
    
    * Image url value must be quoted within a string template (#46)
    
    * chore: remove lock file
    
    * chore: update next-auth@4.0.0-beta.7
    
    * fix: exact version
    
    * Update _middleware.js
    
    * fix: comment out email adapter
    
    * feat: next-auth@4.0.1
    
    * fix: cleanup options
    
    * fix: rm sqlite3
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>
    Co-authored-by: Shawn Goulet <sgoulet@gmail.com>

---
+++ pages/_middleware.js
@@ -0,0 +1,13 @@
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+
+/** @param {import("next/server").NextRequest} req */
+export async function middleware(req) {
+  if (req.nextUrl.pathname === "/middleware-protected") {
+    const session = await getToken({ req, secret: process.env.SECRET })
+    // You could also check for any property on the session object,
+    // like role === "admin" or name === "John Doe", etc.
+    if (!session) return NextResponse.redirect("/api/auth/signin")
+    // If user is authenticated, continue.
+  }
+}
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,18 +1,24 @@
 import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import GoogleProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook"
+import GithubProvider from "next-auth/providers/github"
+import TwitterProvider from "next-auth/providers/twitter"
+import Auth0Provider from "next-auth/providers/auth0"
+// import AppleProvider from "next-auth/providers/apple"
+// import EmailProvider from "next-auth/providers/email"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers
   providers: [
-    Providers.Email({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
+    /* EmailProvider({
+         server: process.env.EMAIL_SERVER,
+         from: process.env.EMAIL_FROM,
+       }),
     // Temporarily removing the Apple provider from the demo site as the
     // callback URL for it needs updating due to Vercel changing domains
-    /*
+      
     Providers.Apple({
       clientId: process.env.APPLE_ID,
       clientSecret: {
@@ -23,38 +29,30 @@ export default NextAuth({
       },
     }),
     */
-    Providers.Facebook({
+    FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
     }),
-    Providers.GitHub({
+    GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
-      scope: "read:user"
+      scope: "read:user",
     }),
-    Providers.Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    Providers.Twitter({
+    TwitterProvider({
       clientId: process.env.TWITTER_ID,
       clientSecret: process.env.TWITTER_SECRET,
     }),
-    Providers.Auth0({
+    Auth0Provider({
       clientId: process.env.AUTH0_ID,
       clientSecret: process.env.AUTH0_SECRET,
       domain: process.env.AUTH0_DOMAIN,
     }),
   ],
-  // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
-  // https://next-auth.js.org/configuration/databases
-  //
-  // Notes:
-  // * You must install an appropriate node_module for your database
-  // * The Email provider requires a database (OAuth providers do not)
-  database: process.env.DATABASE_URL,
-
   // The secret should be set to a reasonably long random string.
   // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
   // a separate secret is defined explicitly for encrypting the JWT.
@@ -63,8 +61,8 @@ export default NextAuth({
   session: {
     // Use JSON Web Tokens for session instead of database sessions.
     // This option can be used with or without a database for users/accounts.
-    // Note: `jwt` is automatically set to `true` if no database is specified.
-    jwt: true,
+    // Note: `strategy` should be set to 'jwt' if no database is used.
+    strategy: 'jwt'
 
     // Seconds - How long until an idle session expires and is no longer valid.
     // maxAge: 30 * 24 * 60 * 60, // 30 days
@@ -81,6 +79,7 @@ export default NextAuth({
   jwt: {
     // A secret to use for key generation (you should set this explicitly)
     // secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw',
+    secret: process.env.SECRET,
     // Set to true to use encryption (default: false)
     // encryption: true,
     // You can define your own encode/decode functions for signing and encryption
@@ -106,10 +105,10 @@ export default NextAuth({
   // when an action is performed.
   // https://next-auth.js.org/configuration/callbacks
   callbacks: {
-    // async signIn(user, account, profile) { return true },
-    // async redirect(url, baseUrl) { return baseUrl },
-    // async session(session, user) { return session },
-    // async jwt(token, user, account, profile, isNewUser) { return token }
+    // async signIn({ user, account, profile, email, credentials }) { return true },
+    // async redirect({ url, baseUrl }) { return baseUrl },
+    // async session({ session, token, user }) { return session },
+    // async jwt({ token, user, account, profile, isNewUser }) { return token }
   },
 
   // Events are useful for logging
@@ -118,7 +117,9 @@ export default NextAuth({
 
   // You can set the theme to 'light', 'dark' or use 'auto' to default to the
   // whatever prefers-color-scheme is set to in the browser. Default is 'auto'
-  theme: 'light',
+  theme: {
+    colorScheme: "light",
+  },
 
   // Enable debug messages in the console if you are having problems
   debug: false,

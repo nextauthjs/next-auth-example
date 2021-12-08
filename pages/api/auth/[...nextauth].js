656fed9f01e436407e81f659c8b71fd6e05dac0d
Balázs Orbán <info@balazsorban.com>
2021-12-08 16:31:49 +0100

    remove `jwt.secret` as top-level should be enough

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -62,7 +62,7 @@ export default NextAuth({
     // Use JSON Web Tokens for session instead of database sessions.
     // This option can be used with or without a database for users/accounts.
     // Note: `strategy` should be set to 'jwt' if no database is used.
-    strategy: 'jwt'
+    strategy: "jwt",
 
     // Seconds - How long until an idle session expires and is no longer valid.
     // maxAge: 30 * 24 * 60 * 60, // 30 days
@@ -77,11 +77,6 @@ export default NextAuth({
   // option is set - or by default if no database is specified.
   // https://next-auth.js.org/configuration/options#jwt
   jwt: {
-    // A secret to use for key generation (you should set this explicitly)
-    // secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw',
-    secret: process.env.SECRET,
-    // Set to true to use encryption (default: false)
-    // encryption: true,
     // You can define your own encode/decode functions for signing and encryption
     // if you want to override the default behaviour.
     // encode: async ({ secret, token, maxAge }) => {},

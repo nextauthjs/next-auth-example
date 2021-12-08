3af7650733b236cb70334c99ada38c93e09fd705
Balázs Orbán <info@balazsorban.com>
2021-12-08 17:07:18 +0100

    add secureCookie option

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -5,7 +5,13 @@ import { NextResponse } from "next/server"
 export async function middleware(req) {
   if (req.nextUrl.pathname === "/middleware-protected") {
     console.log({ cookies: req.cookies, secret: process.env.SECRET })
-    const session = await getToken({ req, secret: process.env.SECRET })
+    const session = await getToken({
+      req,
+      secret: process.env.SECRET,
+      secureCookie:
+        process.env.NEXTAUTH_URL?.startsWith("https://") ??
+        !!process.env.VERCEL_URL,
+    })
     // You could also check for any property on the session object,
     // like role === "admin" or name === "John Doe", etc.
     if (!session) return NextResponse.redirect("/api/auth/signin")

bf319e7736b8086b5fed6b24f78dc48a8997e975
Balázs Orbán <info@balazsorban.com>
2021-12-08 18:31:49 +0100

    add log

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,8 +4,15 @@ import { NextResponse } from "next/server"
 /** @param {import("next/server").NextRequest} req */
 export async function middleware(req) {
   if (req.nextUrl.pathname === "/middleware-protected") {
-    console.log({ cookies: req.cookies, secret: process.env.SECRET })
+    console.log({
+      cookies: req.cookies,
+      secret: process.env.SECRET,
+      secure:
+        process.env.NEXTAUTH_URL?.startsWith("https://") ??
+        !!process.env.VERCEL_URL,
+    })
     const session = await getToken({ req, secret: process.env.SECRET })
+    console.log({ session })
     // You could also check for any property on the session object,
     // like role === "admin" or name === "John Doe", etc.
     if (!session) return NextResponse.redirect("/api/auth/signin")

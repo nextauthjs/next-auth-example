6c144ba87a063139116cbf8c00cf7baaaaa7e009
Balázs Orbán <info@balazsorban.com>
2021-12-08 18:37:00 +0100

    remove log, add `secureCookie` option

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,15 +4,13 @@ import { NextResponse } from "next/server"
 /** @param {import("next/server").NextRequest} req */
 export async function middleware(req) {
   if (req.nextUrl.pathname === "/middleware-protected") {
-    console.log({
-      cookies: req.cookies,
+    const session = await getToken({
+      req,
       secret: process.env.SECRET,
-      secure:
+      secureCookie:
         process.env.NEXTAUTH_URL?.startsWith("https://") ??
         !!process.env.VERCEL_URL,
     })
-    const session = await getToken({ req, secret: process.env.SECRET })
-    console.log({ session })
     // You could also check for any property on the session object,
     // like role === "admin" or name === "John Doe", etc.
     if (!session) return NextResponse.redirect("/api/auth/signin")

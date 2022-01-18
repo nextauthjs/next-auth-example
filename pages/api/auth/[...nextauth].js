c51f1b8f41a15bbc81a077e84a2c1d3fe65e6f24
Balázs Orbán <info@balazsorban.com>
2022-01-18 03:06:13 +0100

    feat: add `withAuth` Middleware example

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -55,7 +55,7 @@ export default NextAuth({
   // The secret should be set to a reasonably long random string.
   // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
   // a separate secret is defined explicitly for encrypting the JWT.
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 
   session: {
     // Use JSON Web Tokens for session instead of database sessions.

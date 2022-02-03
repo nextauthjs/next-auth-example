b3bea39b7d46339d9d43f0457d6f170cc05fca2b
Balázs Orbán <info@balazsorban.com>
2022-02-03 23:29:20 +0100

    specify comment

--- pages/admin/_middleware.ts
+++ pages/admin/_middleware.ts
@@ -1,6 +1,6 @@
 import { withAuth } from "next-auth/middleware"
 
-// Only logged-in users with the "admin" role will be able to see this page.
+// Only logged-in users with the "admin" role will be able to see the pages under the "/admin" path.
 // More on how NextAuth middleware works: https://docs-git-misc-docs-nextauthjs.vercel.app/configuration/nextjs
 export default withAuth({
   callbacks: {

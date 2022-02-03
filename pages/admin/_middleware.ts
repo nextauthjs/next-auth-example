c68fcab7519bd7e3861337d2f8240141df83951a
Balázs Orbán <info@balazsorban.com>
2022-02-03 23:35:11 +0100

    fix middleware docs link

--- pages/admin/_middleware.ts
+++ pages/admin/_middleware.ts
@@ -1,7 +1,7 @@
 import { withAuth } from "next-auth/middleware"
 
 // Only logged-in users with the "admin" role will be able to see the pages under the "/admin" path.
-// More on how NextAuth middleware works: https://docs-git-misc-docs-nextauthjs.vercel.app/configuration/nextjs
+// More on how NextAuth middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 export default withAuth({
   callbacks: {
     authorized: ({ token }) => token?.userRole === "admin",

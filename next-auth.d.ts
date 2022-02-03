2cabf44c7b8549ab9795f0ab67e472d4db66a7e1
Balázs Orbán <info@balazsorban.com>
2022-02-03 23:26:09 +0100

    add comment about module augmentation

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,5 +1,7 @@
 import "next-auth/jwt"
 
+// Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
+
 declare module "next-auth/jwt" {
   interface JWT {
     /** The user's role. */

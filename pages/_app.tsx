fc24ad302b6a470cc0d293bdb16e0fe2a87e86a0
Balázs Orbán <info@balazsorban.com>
2022-02-03 23:28:08 +0100

    change phrasing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
 import "./styles.css"
 
-// Use of the <SessionProvider> is now mandatory to allow components that call
+// Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({ Component, pageProps }: AppProps) {
   return (

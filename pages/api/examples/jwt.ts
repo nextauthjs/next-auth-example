6384c419bbf57a7167cf9ba9678014c3e4f1ed82
Kondtantin Zolin <zolin@energo-kontrol.ru>
2022-02-04 19:00:02 +0700

    Edit variable name after changing api https://next-auth.js.org/configuration/options#nextauth_secret

--- pages/api/examples/jwt.ts
+++ pages/api/examples/jwt.ts
@@ -2,7 +2,7 @@
 import { getToken } from "next-auth/jwt"
 import type { NextApiRequest, NextApiResponse } from "next"
 
-const secret = process.env.SECRET
+const secret = process.env.NEXTAUTH_SECRET​
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const token = await getToken({ req, secret })

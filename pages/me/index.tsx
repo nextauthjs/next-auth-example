00462017a3d42e3722793be73059c4c86c30c507
Balázs Orbán <info@balazsorban.com>
2022-02-04 01:14:01 +0100

    add layout to /me, use canary `next` temporarily

--- pages/me/index.tsx
+++ pages/me/index.tsx
@@ -1,7 +1,12 @@
 import { useSession } from "next-auth/react"
+import Layout from "../../components/layout"
 
 export default function MePage() {
   const { data } = useSession()
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
+  return (
+    <Layout>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </Layout>
+  )
 }

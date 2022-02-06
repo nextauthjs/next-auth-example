614e0395daa24fecade2fbf9cf49d07f1c0263aa
Dominik Ferber <dominik.ferber@gmail.com>
2022-02-06 18:44:10 +0200

    remove invisible space char

--- pages/api/examples/jwt.ts
+++ pages/api/examples/jwt.ts
@@ -2,7 +2,7 @@
 import { getToken } from "next-auth/jwt"
 import type { NextApiRequest, NextApiResponse } from "next"
 
-const secret = process.env.NEXTAUTH_SECRET​
+const secret = process.env.NEXTAUTH_SECRET
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const token = await getToken({ req, secret })

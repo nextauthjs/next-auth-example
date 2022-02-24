027cd4db100d8e342ab1553cc20793ad66666d4c
pramod2 <kandelpramod2@gmail.com>
2022-02-23 23:36:18 -0800

    Code showing that API auth header token is not accessible through middleware but accessible through API code.

---
+++ pages/api/_middleware.ts
@@ -0,0 +1,14 @@
+import { withAuth } from "next-auth/middleware"
+import { NextRequest, NextResponse } from "next/server"
+
+export default withAuth(
+  function middleware(req: NextRequest & { nextauth: { token: JWT } }) {
+    console.log("Middleware token", req.nextauth.token)
+    return NextResponse.next()
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => true,
+    },
+  }
+)
--- pages/api/examples/session.ts
+++ pages/api/examples/session.ts
@@ -1,8 +1,11 @@
 // This is an example of how to access a session from an API route
-import { getSession } from "next-auth/react"
 import type { NextApiRequest, NextApiResponse } from "next"
+import { getToken } from "next-auth/jwt"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req })
-  res.send(JSON.stringify(session, null, 2))
+  // const session = await getSession({ req })
+  const token = await getToken({ req })
+  console.log("Server token", token)
+  res.send(JSON.stringify(token, null, 2))
+  // res.send(JSON.stringify(session, null, 2))
 }

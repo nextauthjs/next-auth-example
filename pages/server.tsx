9b3c2924f4bb813ef5f70642bd0e86fe7311092a
balazsorban44 <info@balazsorban.com>
2023-06-27 12:29:33 +0000

    🔄 Synced local '.' with remote 'apps/example-nextjs'

--- pages/server.tsx
+++ pages/server.tsx
@@ -3,10 +3,9 @@ import { authOptions } from "./api/auth/[...nextauth]"
 import Layout from "../components/layout"
 
 import type { GetServerSidePropsContext } from "next"
-import { useSession } from "next-auth/react"
+import type { Session } from "next-auth"
 
-export default function ServerSidePage() {
-  const { data: session } = useSession()
+export default function ServerSidePage({ session }: { session: Session }) {
   // As this page uses Server Side Rendering, the `session` will be already
   // populated on render without needing to go through a loading stage.
   return (

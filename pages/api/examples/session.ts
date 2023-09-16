60773fcdae5ff7fe15a79a42734bccdbb381e5c4
balazsorban44 <info@balazsorban.com>
2023-09-16 05:08:20 +0000

    🔄 Synced local '.' with remote 'apps/example-nextjs'

--- pages/api/examples/session.ts
+++ pages/api/examples/session.ts
@@ -1,12 +1,13 @@
 // This is an example of how to access a session from an API route
+import { getServerSession } from "next-auth"
+import { authOptions } from "../auth/[...nextauth]"
 
-import { auth } from "auth"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await auth(req, res)
+  const session = await getServerSession(req, res, authOptions)
   res.send(JSON.stringify(session, null, 2))
 }

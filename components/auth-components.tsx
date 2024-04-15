a7592233cb4b5fffc5a60beaa90c362e15e5ad80
VIVEK PATEL <patelvk2000@gmail.com>
2024-04-15 00:26:02 -0400

    try with 14.2.1

--- components/auth-components.tsx
+++ components/auth-components.tsx
@@ -1,34 +1,28 @@
-import { signIn, signOut } from "auth"
-import { Button } from "./ui/button"
+"use client";
+
+import { signIn, signOut } from "next-auth/react";
+import { Button } from "./ui/button";
 
 export function SignIn({
   provider,
   ...props
 }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn(provider)
-      }}
-    >
-      <Button {...props}>Sign In</Button>
-    </form>
-  )
+    <Button {...props} onClick={() => signIn(provider)}>
+      Sign In
+    </Button>
+  );
 }
 
 export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signOut()
-      }}
-      className="w-full"
+    <Button
+      variant="ghost"
+      className="w-full p-0"
+      onClick={() => signOut()}
+      {...props}
     >
-      <Button variant="ghost" className="w-full p-0" {...props}>
-        Sign Out
-      </Button>
-    </form>
-  )
+      Sign Out
+    </Button>
+  );
 }

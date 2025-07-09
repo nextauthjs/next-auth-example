f059998d3117611f0954f22b2a0c550b578342b9
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-07-09 07:11:48 -0500

    MVP F3 Nation Auth App (#1)
    * configure auth
    
    * add pull request template
    
    * add .cursorrules
    
    * add .github/workflows/pr.yaml

---
+++ .github/pull_request_template.md
@@ -0,0 +1,57 @@
+<!--
+Learn more about GitHub Pull Request Templates at...
+https://docs.github.com/en/communities/using-templates-to-encourage-useful-issues-and-pull-requests/creating-a-pull-request-template-for-your-repository
+-->
+<!--
+### 🚧 This PR is Part of a Series
+-->
+<!--
+You can simply remove this section
+if you don't need it for your use-case
+(e.g., a one-off PR that isn't actually
+stacked or part of a series)
+-->
+<!--
+- #1 short description
+- #2 short description
+- #3 short description
+-->
+
+### 👋 TL;DR
+
+<!-- keep it simple, a sentence or two at most -->
+
+(coming soon)
+
+### 🔎 Details
+
+<!--
+This is the place to get in the weeds about what changed.
+Consider linking out to project artifacts like:
+- Jira issue(s)
+- Slack post(s)
+- Loom video(s)
+- Figma design(s)
+- etc.
+-->
+
+(coming soon)
+
+### ✅ How to Test
+
+<!--
+Document what a real testing looks like.
+Think not only about code review,
+but also about QA/UAT.
+Given-When-Then acceptance criteria are great,
+but even just a flat list of common test cases
+in common language is better than nothing
+-->
+
+(coming soon)
+
+### 🥜 GIF
+
+<!-- GIFs are always optional but never forgotten -->
+
+![lack-of-hustle](https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWZ2enV5YXY5YXdzb2IwOWFtMGp1OTd0bGljdHBzNXpiYXVzM2Y2ZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oKIPACZEWen2eaBm8/giphy.gif)
--- auth.ts
+++ auth.ts
@@ -1,38 +1,38 @@
 import NextAuth from "next-auth"
 import "next-auth/jwt"
 
-import Apple from "next-auth/providers/apple"
+// import Apple from "next-auth/providers/apple"
 // import Atlassian from "next-auth/providers/atlassian"
-import Auth0 from "next-auth/providers/auth0"
-import AzureB2C from "next-auth/providers/azure-ad-b2c"
-import BankIDNorway from "next-auth/providers/bankid-no"
-import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
-import Cognito from "next-auth/providers/cognito"
-import Coinbase from "next-auth/providers/coinbase"
-import Discord from "next-auth/providers/discord"
-import Dropbox from "next-auth/providers/dropbox"
-import Facebook from "next-auth/providers/facebook"
-import GitHub from "next-auth/providers/github"
-import GitLab from "next-auth/providers/gitlab"
+// import Auth0 from "next-auth/providers/auth0"
+// import AzureB2C from "next-auth/providers/azure-ad-b2c"
+// import BankIDNorway from "next-auth/providers/bankid-no"
+// import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
+// import Cognito from "next-auth/providers/cognito"
+// import Coinbase from "next-auth/providers/coinbase"
+// import Discord from "next-auth/providers/discord"
+// import Dropbox from "next-auth/providers/dropbox"
+// import Facebook from "next-auth/providers/facebook"
+// import GitHub from "next-auth/providers/github"
+// import GitLab from "next-auth/providers/gitlab"
 import Google from "next-auth/providers/google"
-import Hubspot from "next-auth/providers/hubspot"
-import Keycloak from "next-auth/providers/keycloak"
-import LinkedIn from "next-auth/providers/linkedin"
-import MicrosoftEntraId from "next-auth/providers/microsoft-entra-id"
-import Netlify from "next-auth/providers/netlify"
-import Okta from "next-auth/providers/okta"
-import Passage from "next-auth/providers/passage"
+// import Hubspot from "next-auth/providers/hubspot"
+// import Keycloak from "next-auth/providers/keycloak"
+// import LinkedIn from "next-auth/providers/linkedin"
+// import MicrosoftEntraId from "next-auth/providers/microsoft-entra-id"
+// import Netlify from "next-auth/providers/netlify"
+// import Okta from "next-auth/providers/okta"
+// import Passage from "next-auth/providers/passage"
 import Passkey from "next-auth/providers/passkey"
-import Pinterest from "next-auth/providers/pinterest"
-import Reddit from "next-auth/providers/reddit"
-import Slack from "next-auth/providers/slack"
-import Salesforce from "next-auth/providers/salesforce"
-import Spotify from "next-auth/providers/spotify"
-import Twitch from "next-auth/providers/twitch"
-import Twitter from "next-auth/providers/twitter"
-import Vipps from "next-auth/providers/vipps"
-import WorkOS from "next-auth/providers/workos"
-import Zoom from "next-auth/providers/zoom"
+// import Pinterest from "next-auth/providers/pinterest"
+// import Reddit from "next-auth/providers/reddit"
+// import Slack from "next-auth/providers/slack"
+// import Salesforce from "next-auth/providers/salesforce"
+// import Spotify from "next-auth/providers/spotify"
+// import Twitch from "next-auth/providers/twitch"
+// import Twitter from "next-auth/providers/twitter"
+// import Vipps from "next-auth/providers/vipps"
+// import WorkOS from "next-auth/providers/workos"
+// import Zoom from "next-auth/providers/zoom"
 import { createStorage } from "unstorage"
 import memoryDriver from "unstorage/drivers/memory"
 import vercelKVDriver from "unstorage/drivers/vercel-kv"
@@ -50,33 +50,33 @@ const storage = createStorage({
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   debug: !!process.env.AUTH_DEBUG,
-  theme: { logo: "https://authjs.dev/img/logo-sm.png" },
+  theme: { logo: "https://storage.googleapis.com/msgsndr/SrfvOYstGSlBjAXxhvwX/media/660077cd85c11a5aa4c3953b.webp" },
   adapter: UnstorageAdapter(storage),
   providers: [
-    Apple,
+    // Apple,
     // Atlassian,
-    Auth0,
-    AzureB2C,
-    BankIDNorway,
-    BoxyHQSAML({
-      clientId: "dummy",
-      clientSecret: "dummy",
-      issuer: process.env.AUTH_BOXYHQ_SAML_ISSUER,
-    }),
-    Cognito,
-    Coinbase,
-    Discord,
-    Dropbox,
-    Facebook,
-    GitHub,
-    GitLab,
+    // Auth0,
+    // AzureB2C,
+    // BankIDNorway,
+    // BoxyHQSAML({
+    //   clientId: "dummy",
+    //   clientSecret: "dummy",
+    //   issuer: process.env.AUTH_BOXYHQ_SAML_ISSUER,
+    // }),
+    // Cognito,
+    // Coinbase,
+    // Discord,
+    // Dropbox,
+    // Facebook,
+    // GitHub,
+    // GitLab,
     Google,
-    Hubspot,
-    Keycloak({ name: "Keycloak (bob/bob)" }),
-    LinkedIn,
-    MicrosoftEntraId,
-    Netlify,
-    Okta,
+    // Hubspot,
+    // Keycloak({ name: "Keycloak (bob/bob)" }),
+    // LinkedIn,
+    // MicrosoftEntraId,
+    // Netlify,
+    // Okta,
     Passkey({
       formFields: {
         email: {
@@ -86,19 +86,19 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         },
       },
     }),
-    Passage,
-    Pinterest,
-    Reddit,
-    Salesforce,
-    Slack,
-    Spotify,
-    Twitch,
-    Twitter,
-    Vipps({
-      issuer: "https://apitest.vipps.no/access-management-1.0/access/",
-    }),
-    WorkOS({ connection: process.env.AUTH_WORKOS_CONNECTION! }),
-    Zoom,
+    // Passage,
+    // Pinterest,
+    // Reddit,
+    // Salesforce,
+    // Slack,
+    // Spotify,
+    // Twitch,
+    // Twitter,
+    // Vipps({
+    //   issuer: "https://apitest.vipps.no/access-management-1.0/access/",
+    // }),
+    // WorkOS({ connection: process.env.AUTH_WORKOS_CONNECTION! }),
+    // Zoom,
   ],
   basePath: "/auth",
   session: { strategy: "jwt" },
